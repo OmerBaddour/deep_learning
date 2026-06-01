7a36bad54b3c29193d8a78fb93b3eb21ed7e8629
OmerBaddour <omer.m.baddour@gmail.com>
2026-05-31 23:39:22 -0400

    backwards

--- src/deep_learning/value.py
+++ src/deep_learning/value.py
@@ -26,6 +26,9 @@ class Value:
     self.children = [] if children is None else children
     self.gradient = gradient
 
+  def __repr__(self) -> str:
+    return f'{self.__class__.__name__}({self.__dict__})'
+
   def __add__(self, other: float | int) -> Value:
     other_value = None
     if isinstance(other, Value):
@@ -36,12 +39,12 @@ class Value:
       return NotImplemented
 
     return Value(
-        data=self.data + other.data,
+        data=self.data + other_value.data,
         op=Op.PLUS,
-        children=[self, other],
+        children=[self, other_value],
     )
 
-  def __mult__(self, other: float | int) -> Value:
+  def __mul__(self, other: float | int) -> Value:
     other_value = None
     if isinstance(other, Value):
       other_value = other
@@ -51,20 +54,61 @@ class Value:
       return NotImplemented
     
     return Value(
-        data=self.data * other.data,
+        data=self.data * other_value.data,
         op=Op.MULTIPLY,
-        children=[self, other],
+        children=[self, other_value],
     )
   
   def forward(self) -> float:
     if len(self.children) == 0:
       return self.data
     else:
-      return eval(self.op.value.join(str(value.forward()) for value in self.children))
+      self.data = eval(self.op.value.join(str(value.forward()) for value in self.children))
+      return self.data
 
   def backward(self) -> None:
-    pass
+    # topologically sort graph
+    topologically_sorted_graph: list[Value] = []
+    
+    def do_topological_sort(node: Value) -> None:
+      # NOTE: assume acyclic for simplicity
+      if len(node.children) == 0:
+        if node not in topologically_sorted_graph:
+          topologically_sorted_graph.append(node)
+      else:
+        for child in node.children:
+          do_topological_sort(child)
+        if node not in topologically_sorted_graph:
+          topologically_sorted_graph.append(node)
+    do_topological_sort(self)
 
+    for node in reversed(topologically_sorted_graph):
+      '''
+      The chain rule tells us how to compute the derivative of z = f(y), where y = g(x): https://en.wikipedia.org/wiki/Chain_rule
+      dz/dx = dz/dy * dy/dx
+      We call dy/dx the "local derivative"
+
+      Limit definition of derivative: https://en.wikipedia.org/wiki/Derivative#As_a_limit
+      Let y = f(a), then dy/da = lim h -> 0 of (f(a + h) - f(a)) / f(h)
+      We can algebraically derive that
+      - if f(a) = a + <stuff>, dy/da = 1.0
+      - if f(a) = a * <number>, dy/da = <number>
+
+      Final note: in the multivariate case: https://en.wikipedia.org/wiki/Chain_rule#Example:_arithmetic_operations
+      we do += to the gradient 
+      '''
+      if len(node.children) > 0:
+        if node.op == Op.PLUS:
+          for child in node.children:
+            local_derivative = 1.0
+            child.gradient += local_derivative * node.gradient
+        elif node.op == Op.MULTIPLY:
+          for i, child in enumerate(node.children):
+            other_children_data = [other_child.data for j, other_child in enumerate(node.children) if i != j]
+            local_derivative = eval(Op.MULTIPLY.value.join(str(other_child_data) for other_child_data in other_children_data))
+            child.gradient += local_derivative * node.gradient
+        else:
+          raise NotImplementedError(f'{node.op} not supported')
 
 def draw(root: Value) -> Digraph:
   dot = Digraph(graph_attr={'rankdir': 'LR'})
--- tests/test_value.py
+++ tests/test_value.py
@@ -18,12 +18,71 @@ def simple_value_graph() -> Value:
 
 def test_basic():
   assert Value(1).data == 1.0
+  
+  x = Value(1) + 2
+  assert isinstance(x, Value) and x.data == 3.0 and x.op == Op.PLUS
+  
+  x = Value(1) * 2
+  assert isinstance(x, Value) and x.data == 2.0 and x.op == Op.MULTIPLY
 
 def test_forward(simple_value_graph: Value):
-  assert simple_value_graph.forward() == 35.0
+  L = simple_value_graph
+
+  f = L.children[1]
+  e = f.children[1]
+  d = f.children[0]
+  
+  c = L.children[0]
+  b = c.children[1]
+  a = c.children[0]
+
+  assert L.forward() == 35.0
+
+  assert a.data == 2.0
+  assert b.data == 3.0
+  assert c.data == a.data + b.data
+  
+  assert d.data == 5.0
+  assert e.data == 6.0
+  assert f.data == d.data * e.data
+
+  assert L.data == c.data + f.data
 
 def test_draw(simple_value_graph: Value):
   draw(simple_value_graph)
 
-def test_backward():
-  pass
+def test_backward(simple_value_graph: Value):
+  L = simple_value_graph
+  L.forward()
+
+  L.gradient = 1.0
+  L.backward()
+
+  f = L.children[1]
+  e = f.children[1]
+  d = f.children[0]
+  
+  c = L.children[0]
+  b = c.children[1]
+  a = c.children[0]
+
+  assert L.gradient == 1.0
+  
+  assert c.gradient == 1.0
+  assert f.gradient == 1.0
+  
+  assert a.gradient == 1.0
+  assert b.gradient == 1.0
+
+  assert d.gradient == e.data
+  assert e.gradient == d.data
+
+def test_backward_multi_reference():
+  x = Value(3.0)
+  y: Value = x * x
+  y.forward()
+
+  y.gradient = 1.0
+  y.backward()
+
+  assert x.gradient == x.data * 2
